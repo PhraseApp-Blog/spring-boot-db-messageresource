c318065ef47ca72dd9afef5a7f2efb4b5ea051e6
Administrator <Administrator@DESKTOP-3A2KOHR>
2019-02-03 11:36:04 +0700

    [tlinh2110] Remove the Service

--- src/main/java/com/phraseapp/i18n/dynamiclanguage/repository/LanguageRepository.java
+++ src/main/java/com/phraseapp/i18n/dynamiclanguage/repository/LanguageRepository.java
@@ -1,7 +1,5 @@
 package com.phraseapp.i18n.dynamiclanguage.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +7,7 @@ import com.phraseapp.i18n.dynamiclanguage.entity.LanguageEntity;
 
 @Repository
 public interface LanguageRepository extends JpaRepository<LanguageEntity, Integer> {
-
-	List<LanguageEntity> findAllByLocale(String locale);
+	
+	LanguageEntity findByKeyAndLocale(String key, String locale);
 
 }
--- src/main/java/com/phraseapp/i18n/dynamiclanguage/resource/DBMessageSource.java
+++ src/main/java/com/phraseapp/i18n/dynamiclanguage/resource/DBMessageSource.java
@@ -2,25 +2,25 @@ package com.phraseapp.i18n.dynamiclanguage.resource;
 
 import java.text.MessageFormat;
 import java.util.Locale;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.AbstractMessageSource;
 import org.springframework.stereotype.Component;
 
-import com.phraseapp.i18n.dynamiclanguage.repository.LanguageService;
+import com.phraseapp.i18n.dynamiclanguage.entity.LanguageEntity;
+import com.phraseapp.i18n.dynamiclanguage.repository.LanguageRepository;
 
 @Component("messageSource")
 public class DBMessageSource extends AbstractMessageSource {
 
 	@Autowired
-	private LanguageService langService;
+	private LanguageRepository languageRepository;
 
 	@Override
 	protected MessageFormat resolveCode(String code, Locale locale) {
-		Map<String, String> messages = langService.getAllMessageByLocale(locale.getLanguage());
-		if (messages.containsKey(code)) {
-			return new MessageFormat(messages.get(code), locale);
+		LanguageEntity message = languageRepository.findByKeyAndLocale(code,locale.getLanguage());
+		if (message != null) {
+			return new MessageFormat(message.getContent(), locale);
 		}
 		return new MessageFormat("", locale);
 	}
